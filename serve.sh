0b0c4490eddb0eb6ac7f0011ec8bb1105d8fb729
joe2far <joe2farrell@gmail.com>
2016-09-05 19:01:29 +0100

    Add script to locally serve dashboard from docker container

---
+++ serve.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 
+# Serve dashboard locally (NB: first run "kubectl proxy --port=8080")
+#
+
+# Docker options
+DOCKER_RUN_OPTS=${DOCKER_RUN_OPTS:-}
+DASHBOARD_IMAGE_NAME="kubernetes-dashboard-build-image"
+DEFAULT_COMMAND=${DEFAULT_COMMAND:-"node_modules/.bin/gulp"}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Serve dashboard locally
+sudo docker run \
+	-it \
+	--rm \
+	--net=host \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v ${DIR}/src/app/frontend/:/dashboard/src/app/frontend/ \
+	${DOCKER_RUN_OPTS} \
+	${DASHBOARD_IMAGE_NAME} \
+	${DEFAULT_COMMAND} serve
